d03c696da378caa382bbd81e992cc4069a33bba3
Miya-village <m.miyamura.23.10.1184@4nonome.com>
2023-12-06 06:00:34 +0900

    1を素数ではないと判定するように修正

--- prime_number.py
+++ prime_number.py
@@ -6,19 +6,23 @@ a = int(a)
 b = int(b)
 
 def f(n):
-    s = 0
-    i = 1
-    while i <= (n ** (1/2)): #√n以下の自然数でnを割り切れるか試す
-        i = i + 1
-        if n % i == 0:
-            s = 1
-            break
-
-    if s == 0:
-        print(f"{n}" + "は素数です")
+    if n == 1:
+        print("1は素数ではありません")
     
     else:
-        print(f"{n}" + "は素数ではありません")
+        s = 0
+        i = 1
+        while i <= (n ** (1/2)): #√n以下の自然数でnを割り切れるか試す
+            i = i + 1
+            if n % i == 0:
+                s = 1
+                break
+
+        if s == 0:
+            print(f"{n}" + "は素数です")
+    
+        else:
+            print(f"{n}" + "は素数ではありません")
 
 f(a)
 f(b)
