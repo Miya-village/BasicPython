b24137ea5239a91d7827b8bbfd90c80795ced10c
Miya-village <m.miyamura.23.10.1184@4nonome.com>
2023-12-05 15:42:40 +0900

    Python入門

--- pi.py
+++ pi.py
@@ -4,4 +4,12 @@ text = """
 """
 
 # TODO
+text_1 = [x.strip(",") for x in text.split()] #textを空白で区切りカンマを消去
+text_2 = [x.strip(".") for x in text_1]       #ピリオドを消去
+text_c = list(map(len, text_2))               #それぞれの文字数をカウント
 
+result = str(text_c[0])
+for i in range(1, len(text_c)):
+    result += str(text_c[i])
+
+print(result)
